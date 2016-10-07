93dab625e039344308308a36a2251e16b9a1c6f5
Dan Nelson <dan@luxusgroup.com>
2016-10-07 13:02:33 -0600

    Trying to put autosize in the ember-cli-build.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'ember-cli-textarea-autosize',
   included: function (app) {
-    this._super.included(app);
-    app.import(app.bowerDirectory + '/autosize/dist/autosize.js');
-  }
-};
+      this._super.included(app);
+      // app.import(app.bowerDirectory + '/autosize/dist/autosize.js');
+    }
+  };
