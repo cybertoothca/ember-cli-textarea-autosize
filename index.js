de49133d03819dc8f888452af666966bcc3f7c73
Dan Nelson <dan@luxusgroup.com>
2016-10-07 13:40:12 -0600

    Created a blueprint to install autosize for me after the addon is installed

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ module.exports = {
   name: 'ember-cli-textarea-autosize',
   included: function (app) {
       this._super.included(app);
-      // app.import(app.bowerDirectory + '/autosize/dist/autosize.js');
+      app.import(app.bowerDirectory + '/autosize/dist/autosize.js');
     }
   };
