650e602e284d9e649009544e80adab6663944c48
Dan Nelson <dan@luxusgroup.com>
2016-10-07 13:47:18 -0600

    Changed the name of the blueprint

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-textarea-autosize',
-  included: function (app) {
-      this._super.included(app);
-      app.import(app.bowerDirectory + '/autosize/dist/autosize.js');
-    }
+  name: 'ember-cli-textarea-autosize'
   };
