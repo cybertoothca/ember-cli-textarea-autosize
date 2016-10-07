5d73214fa32737910a8ff0effd84f89b13dceed1
Dan Nelson <dan@luxusgroup.com>
2016-10-07 14:06:23 -0600

    Fixing some typos and syntax errors

--- blueprints/ember-cli-textarea-autosize/index.js
+++ blueprints/ember-cli-textarea-autosize/index.js
@@ -1,8 +1,8 @@
 /*jshint node:true*/
 module.exports = {
-  description: 'The blueprint that adds the autosize bower library to the project.'
-
-  afterInstall: function(options) {
+  description: 'The blueprint that adds the autosize bower library to the project.',
+  normalizeEntityName: function () {},
+  afterInstall: function () {
     return this.addBowerPackageToProject('autosize');
   }
 };
