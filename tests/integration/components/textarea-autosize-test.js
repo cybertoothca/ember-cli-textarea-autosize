fa9f2027e10bcbdfa1fdb601c59ddf761a0a0089
Dan Nelson <dnelson@cybertooth.ca>
2016-10-08 01:20:53 -0600

    README & min-height & max-height (#2)
    * Created the textarea component that extends Ember.TextArea
    
    * Trying to put autosize in the ember-cli-build.js
    
    * Created a blueprint to install autosize for me after the addon is installed
    
    * Changed the name of the blueprint
    
    * Actually add the blueprint to git this time.
    
    * Fixing some typos and syntax errors
    
    * Passing the correct instance to autosize
    
    * Updated the README and are permitting min-height and max-height.

--- tests/integration/components/textarea-autosize-test.js
+++ tests/integration/components/textarea-autosize-test.js
@@ -1,17 +1,23 @@
-import {
-  moduleForComponent,
-  test
-} from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('textarea-autosize', 'Integration | Component | textarea autosize', {
   integration: true
 });
 
+test('when rendering a min-width', function (assert) {
+  this.render(hbs `{{textarea-autosize min-height="200px"}}`);
+  assert.equal(this.$('textarea').css('min-height'), '200px');
+});
+
+test('when rendering a max-width', function (assert) {
+  this.render(hbs `{{textarea-autosize max-height="500px"}}`);
+  assert.equal(this.$('textarea').css('max-height'), '500px');
+});
+
 test('when rendering without a block', function (assert) {
   this.render(hbs `{{textarea-autosize value="Some text"}}`);
   assert.equal(this.$('textarea').val().trim(), 'Some text');
-  // assert.equal(this.$('textarea').height(), 44);
 });
 
 test('when rendering with a block', function (assert) {
