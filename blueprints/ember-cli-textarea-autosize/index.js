49ac9c876820580f4b6558fdcb179e1c12ea5c6f
Dan Nelson <dnelson@cybertooth.ca>
2016-10-22 23:12:24 -0600

    Readme improvements (#8)
    * Improve README with addon testing notes and a version badge in the project title
    
    * Added the dependencies badge
    
    * README improvements
    
    * Fix the readme

--- blueprints/ember-cli-textarea-autosize/index.js
+++ blueprints/ember-cli-textarea-autosize/index.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'The blueprint that adds the autosize bower library to the project.',
   normalizeEntityName: function () {
   },
-  afterInstall: function () {
+  afterInstall: function(/*options*/) {
     return this.addBowerPackagesToProject([
       {name: 'autosize'},
       {name: 'keyevent'}
