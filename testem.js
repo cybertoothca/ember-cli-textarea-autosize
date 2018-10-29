7125e669552a402c74740cfb7121184dc56d0d22
Dan Nelson <dnelson@cybertooth.ca>
2018-10-28 22:07:26 -0600

    Upgrading to CircleCi-2

--- testem.js
+++ testem.js
@@ -1,19 +1,48 @@
 /* eslint-env node */
-module.exports = {
-  test_page: 'tests/index.html?hidepassed',
+
+/**
+ * By default, your tests will run in Chrome.
+ *
+ * To run your tests in headless (FASTER) mode (launch_in_ci):
+ *    `ember t`
+ *
+ * To run your tests with the browser test-page summary (launch_in_dev):
+ *    `ember t -s`
+ *
+ */
+const isHeadless = !process.argv.includes('-s');
+
+let options = {
+  framework: 'qunit',
+  test_page: 'tests/index.html?hidepassed&nocontainer&notifications',
   disable_watching: true,
+  /**
+   * `ember t` run ci-mode
+   */
   launch_in_ci: [
     'Chrome'
   ],
+  /**
+   * Seems `launch_in_dev` seems to fire when you run `ember t -s`
+   */
   launch_in_dev: [
     'Chrome'
-  ],
-  browser_args: {
+  ]
+};
+
+if (isHeadless) {
+  options.browser_args = {
     Chrome: [
       '--headless',
       '--window-size=1440,900',
       '--disable-gpu',
       '--remote-debugging-port=9222'
+    ],
+    Firefox: [
+      '-headless',
+      '--window-size=1440,900'
     ]
-  }
-};
+  };
+}
+
+module.exports = options;
