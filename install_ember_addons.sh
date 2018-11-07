710eb9d83e36d74726219de3c5240a3b6a57ccbb
Dan Nelson <dan@cybertooth.io>
2018-11-06 22:22:52 -0700

    #22 Ensuring that the old ember-cli-text-support-mixins of 1.x is used.

--- install_ember_addons.sh
+++ install_ember_addons.sh
@@ -6,7 +6,7 @@ ember install ember-cli-deploy-build
 ember install ember-cli-deploy-gzip
 ember install ember-cli-deploy-s3
 ember install ember-cli-font-awesome-sass
-ember install ember-cli-text-support-mixins
+ember install ember-cli-text-support-mixins@~1
 
 npm install --save-dev bootswatch@3.3
 npm install --save autosize@3
