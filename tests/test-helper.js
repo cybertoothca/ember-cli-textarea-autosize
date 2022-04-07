d7574e05d0fe3945ffdf26e7e23135659acbb65d
Dan Nelson <dan@cybertooth.io>
2022-04-07 00:27:27 -0600

    ⬆️ Upgrading To Ember-Cli 3.1 (#27)
    * :recycle: Preparing For Upgrades
    
    * :heavy_plus_sign: Ember-Cli-Update
    
    * :arrow_up: Ember-Cli 2.14...2.15
    
    * v2.15.0
    
    * :arrow_up: Ember-Cli 2.15...2.16
    
    * :arrow_up: Ember-Cli 2.16...2.17
    
    * v2.17.0
    
    * :arrow_up: Ember-Cli 2.17...2.18
    
    * :heavy_plus_sign: Ember-Cli-Update
    
    * :recycle: Apply Code Mods
    
    * :coffin: Remove Bower
    
    * v2.18.0
    
    * :arrow_up: Ember-Cli 2.18...3.0
    
    * v3.0.0
    
    * :arrow_up: Ember-Cli 3.0...3.1
    
    * v3.1.0
    
    * :arrow_up: Upgrade NodeJs

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,10 @@
-import resolver from './helpers/resolver';
-import {setResolver} from 'ember-qunit';
-import {start} from 'ember-cli-qunit';
+import { start } from "ember-qunit";
 
-setResolver(resolver);
+import { setApplication } from "@ember/test-helpers";
+
+import Application from "../app";
+import config from "../config/environment";
+
+setApplication(Application.create(config.APP));
+
 start();
