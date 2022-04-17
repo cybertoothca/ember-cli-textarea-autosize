b51acb4d6e3b0f86ccdb6de834f81c827ba3e318
nadnoslen <dan@cybertooth.io>
2022-04-16 23:11:08 -0600

    :fire: No More JQuery

--- addon/mixins/autosize.js
+++ addon/mixins/autosize.js
@@ -1,8 +1,7 @@
+import { get } from "@ember/object";
+import Mixin from "@ember/object/mixin";
 /* global autosize */
-import { isPresent } from '@ember/utils';
-
-import { on } from '@ember/object/evented';
-import Mixin from '@ember/object/mixin';
+import { isPresent } from "@ember/utils";
 
 /**
  * A mixin for attaching autosize to a component's element.  Is also capable of reading the `min-height` and
@@ -14,26 +13,28 @@ export default Mixin.create({
    * Once this textarea is being destroyed let's help clean up the DOM by removing the autosize binding.
    * @see http://www.jacklmoore.com/autosize/
    */
-  _removeAutosize: on('willDestroyElement', function () {
+  willDestroyElement() {
     autosize.destroy(this.$());
-  }),
+  },
+
   /**
    * Once this textarea is inserted in the DOM initialize on autosize.
    * @see https://github.com/jackmoore/autosize
    */
-  _initializeAutosize: on('didInsertElement', function () {
+  didInsertElement() {
     autosize(this.$());
-    this._setCss('min-height');
-    this._setCss('max-height');
-  }),
+    this._setCss("min-height");
+    this._setCss("max-height");
+  },
+
   /**
-   * Set the jquery css property `propertyName` with the component property `propertyName`.
+   * Set the css property `propertyName` with the component property `propertyName`, e.g. `min-height`.
    * @param propertyName the css property.
    * @private
    */
   _setCss(propertyName) {
-    if (isPresent(this.get(propertyName))) {
-      this.$().css(propertyName, this.get(propertyName));
+    if (isPresent(get(this, propertyName))) {
+      this.element.style[propertyName] = get(this, propertyName);
     }
-  }
+  },
 });
