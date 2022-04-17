636feeeb0ecf9a6f8826ef54dcd68f808ed52ae7
nadnoslen <dan@cybertooth.io>
2022-04-16 23:22:47 -0600

    :recycle: Running Codemods

--- tests/dummy/app/routes/demo-ctrl-enter-submits.js
+++ tests/dummy/app/routes/demo-ctrl-enter-submits.js
@@ -3,8 +3,8 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   actions: {
     submitForm() {
-      this.set('submitCount', this.get('submitCount') + 1);
-      this.get('controller').set('submitMessage', `Submit Count: ${this.get('submitCount')}`);
+      this.set('submitCount', this.submitCount + 1);
+      this.controller.set('submitMessage', `Submit Count: ${this.submitCount}`);
     },
   },
   submitCount: 0,
--- tests/unit/mixins/autosize-test.js
+++ tests/unit/mixins/autosize-test.js
@@ -2,11 +2,11 @@ import EmberObject from '@ember/object';
 import AutosizeMixin from 'ember-cli-textarea-autosize/mixins/autosize';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | autosize');
-
-// Replace this with your real tests.
-test('it works', function (assert) {
-  let AutosizeObject = EmberObject.extend(AutosizeMixin);
-  let subject = AutosizeObject.create();
-  assert.ok(subject);
+module('Unit | Mixin | autosize', function() {
+  // Replace this with your real tests.
+  test('it works', function (assert) {
+    let AutosizeObject = EmberObject.extend(AutosizeMixin);
+    let subject = AutosizeObject.create();
+    assert.ok(subject);
+  });
 });
