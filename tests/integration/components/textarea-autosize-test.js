7598814ce9affbcb1faef7f792108c57f3c2b1b8
Dan Nelson <dan@cybertooth.io>
2022-04-16 23:56:21 -0600

    ⬆️ Upgrading To Ember-Cli 3.8 (#47)
    * v3.4.0
    
    * :arrow_up: v3.4.4...v3.8.3

--- tests/integration/components/textarea-autosize-test.js
+++ tests/integration/components/textarea-autosize-test.js
@@ -3,32 +3,32 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | textarea autosize', function(hooks) {
+module('Integration | Component | textarea autosize', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('when rendering a min-width', async function(assert) {
+  test('when rendering a min-width', async function (assert) {
     await render(hbs`{{textarea-autosize min-height="200px"}}`);
     assert.equal(this.$('textarea').css('min-height'), '200px');
   });
 
-  test('when rendering a max-width', async function(assert) {
+  test('when rendering a max-width', async function (assert) {
     await render(hbs`{{textarea-autosize max-height="500px"}}`);
     assert.equal(this.$('textarea').css('max-height'), '500px');
   });
 
-  test('when rendering some sort of text value', async function(assert) {
+  test('when rendering some sort of text value', async function (assert) {
     await render(hbs`{{textarea-autosize value="Some text"}}`);
     assert.equal(find('textarea').value.trim(), 'Some text');
   });
 
-  test('when supplying undefined the textarea value is empty string', async function(assert) {
+  test('when supplying undefined the textarea value is empty string', async function (assert) {
     this.set('value', undefined);
     await render(hbs`{{textarea-autosize value=value}}`);
 
     assert.dom('textarea').hasValue('');
   });
 
-  test('when supplying null the textarea value is empty string', async function(assert) {
+  test('when supplying null the textarea value is empty string', async function (assert) {
     this.set('value', null);
     await render(hbs`{{textarea-autosize value=value}}`);
 
--- tests/unit/mixins/autosize-test.js
+++ tests/unit/mixins/autosize-test.js
@@ -2,7 +2,7 @@ import EmberObject from '@ember/object';
 import AutosizeMixin from 'ember-cli-textarea-autosize/mixins/autosize';
 import { module, test } from 'qunit';
 
-module('Unit | Mixin | autosize', function() {
+module('Unit | Mixin | autosize', function () {
   // Replace this with your real tests.
   test('it works', function (assert) {
     let AutosizeObject = EmberObject.extend(AutosizeMixin);
